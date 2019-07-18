b0185d03f8d56d236140785281b03be644d0df21
Oueslati <medmelek007@gmail.com>
2019-07-18 19:52:49 +0100

    From Control

--- src/app/fromcontrol/fromcontrol.component.ts
+++ src/app/fromcontrol/fromcontrol.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import {Hero} from '../model/Hero';
 
 @Component({
   selector: 'app-fromcontrol',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FromcontrolComponent implements OnInit {
 
+  hero : Hero = {
+    id : 1 ,
+    name : 'med melek'
+  }
+  name = new FormControl(this.hero.name) ;
+  isName = false ;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  updateName() {
+    console.log(this.name.value)
+  }
+
+  namechange() {
+    console.log(this.name.value)
+    if (this.name.value != this.hero.name) {
+      this.isName=true ;
+    }else{
+      this.isName = false ;
+    }
   }
 
 }
